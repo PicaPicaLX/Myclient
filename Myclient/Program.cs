6f9b537e8a8f65a3662a9c2cbee0b41d85d6e021
栗鑫 <lx389029980@outlook.com>
2018-02-08 21:55:51 +0800

    V1

--- Myclient/Program.cs
+++ Myclient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyCodeLibrary;
 
 namespace Myclient
 {
@@ -10,6 +11,11 @@ namespace Myclient
     {
         static void Main(string[] args)
         {
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapeWebpage("http://www.2345.com", @"C:\Users\lx389\Documents\reply.txt");
+            //string value1 = myScrape.ScrapeWebpage("http://www.2345.com"); // 由于ScrapeWebpage方法有两个重载，所以可以以两种方式发挥作用
+            Console.WriteLine(value);
+            Console.ReadLine();
         }
     }
 }
